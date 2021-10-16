dadcd4a51413569a3f481c1061b4d5ddaa1c6964
laurentiumiu <miulaurctin@gmail.com>
2021-10-16 15:12:40 +0300

    1 commit

---
+++ src/main/java/com/bitcoin/miningtest/MiningtestApplication.java
@@ -0,0 +1,13 @@
+package com.bitcoin.miningtest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiningtestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MiningtestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bitcoin/miningtest/MiningtestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bitcoin.miningtest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MiningtestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
